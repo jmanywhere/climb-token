e270b4f02ff1cd592c39d760150dac0015ea4e55
Semi Invader <=>
2023-04-13 10:54:21 -0600

    erased owner for OWNABLE, tweaked inputs

--- contracts/interfaces/IClimb.sol
+++ contracts/interfaces/IClimb.sol
@@ -6,15 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IClimb is IERC20 {
     function burn(uint256 amount) external;
 
-    function burn() external payable;
-
     function sell(uint256 amount, address stable) external;
 
     function sell(address recipient, uint256 amount, address stable) external;
 
-    function getUnderlyingAsset() external returns (address);
-
-    function buy(uint256 numTokens, address stable) external returns (bool);
+    function buy(uint256 numTokens, address stable) external returns (uint256);
 
     function buy(
         address recipient,
@@ -24,9 +20,39 @@ interface IClimb is IERC20 {
 
     function eraseHoldings(uint256 nHoldings) external;
 
-    function transferOwnership(address newOwner) external;
-
     function volumeFor(address wallet) external view returns (uint256);
 
-    function owner() external view returns (address);
+    ///@notice this function is called by OWNER only and is used to exchange the complete balance in STABLE1 for STABLE2
+    function exchangeTokens(address stable1, address stable2) external;
+
+    ///////////////////////////////////
+    //////        EVENTS        ///////
+    ///////////////////////////////////
+
+    event UpdateShares(uint256 updatedDevShare, uint256 updatedLiquidityShare);
+    event UpdateFees(
+        uint256 updatedSellFee,
+        uint256 updatedMintFee,
+        uint256 updatedTransferFee
+    );
+    event UpdateDevAddress(address updatedDev);
+    event SetMatrixContract(address newMatrix, bool exempt);
+    event PriceChange(
+        uint256 previousPrice,
+        uint256 currentPrice,
+        uint256 totalSupply
+    );
+    event ErasedHoldings(address who, uint256 amountTokensErased);
+    event GarbageCollected(uint256 amountTokensErased);
+    event UpdateTokenSlippage(uint256 newSlippage);
+    event TransferOwnership(address newOwner);
+    event TokenStaked(uint256 assetsReceived, address recipient);
+    event SetFeeExemption(address Contract, bool exempt);
+    event TokenActivated(uint256 totalSupply, uint256 price, uint256 timestamp);
+    event TokenSold(
+        uint256 amountCLIMB,
+        uint256 assetsRedeemed,
+        address recipient
+    );
+    event TokenPurchased(uint256 assetsReceived, address recipient);
 }
