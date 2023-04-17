030953bb37ae83b5c8e94dd287126785762beb60
Semi Invader <=>
2023-04-17 17:21:42 -0600

    tests pass

--- tests/test_climbv2.py
+++ tests/test_climbv2.py
@@ -1,5 +1,7 @@
-from ape import project, reverts
+from ape import project, reverts, Contract
 from pytest import fixture
+
+router = "0x10ED43C718714eb63d5aA57B78B54704E256024E"
 
 
 @fixture
@@ -9,17 +11,34 @@ def setup(accounts):
     user2 = accounts[2]
     user3 = accounts[3]
 
-    usdt = project.TestToken.deploy(sender=owner)
-    busd = project.TestToken.deploy(sender=owner)
+    usdt = Contract("0x55d398326f99059fF775485246999027B3197955")
+    busd = Contract("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56")
     climb = project.ClimbTokenV2.deploy([usdt.address], sender=owner)
+
+    busd_whale = accounts["0xf977814e90da44bfa03b6295a0616a897441acec"]
+    usdt_whale = accounts["0x8894e0a0c962cb723c1976a4421c95949be2d4e3"]
     # THIS NEEDS TO HAPPEN
-    usdt.transfer(climb.address, int(1e18), sender=owner)
+    usdt.transfer(climb.address, int(1e18), sender=usdt_whale)
 
-    yield owner, usdt, busd, climb, user1, user2, user3
+    yield owner, usdt, busd, climb, user1, user2, user3, busd_whale, usdt_whale
 
 
-def test_set_stable_token(setup, accounts):
-    owner, usdt, busd, climb, user1, user2, user3 = setup
+def test_exchange_tokens(setup):
+    owner, usdt, busd, climb, user1, user2, user3, *_ = setup
+
+    current_supply = climb.totalSupply()
+    climb.setStableToken(busd.address, True, sender=owner)
+
+    climb.exchangeTokens(usdt.address, busd.address, router, sender=owner)
+
+    assert usdt.balanceOf(climb.address) == 0
+    assert busd.balanceOf(climb.address) >= int(1e18 * 0.95)
+    assert climb.totalSupply() == current_supply
+    pass
+
+
+def test_set_stable_token(setup):
+    owner, usdt, busd, climb, user1, *_ = setup
 
     with reverts("Ownable: caller is not the owner"):
         climb.setStableToken(usdt.address, True, sender=user1)
@@ -38,7 +57,7 @@ def test_set_stable_token(setup, accounts):
     with reverts("Balance not zero"):
         climb.setStableToken(usdt.address, False, sender=owner)
 
-    climb.exchangeTokens(usdt.address, busd.address, sender=owner)
+    climb.exchangeTokens(usdt.address, busd.address, router, sender=owner)
     climb.setStableToken(usdt.address, False, sender=owner)
 
     assert climb.currentStables(0) == busd.address
