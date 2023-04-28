af73319d33f57a23a5124da1a8445507876120d5
Semi Invader <=>
2023-04-28 10:11:05 -0600

    needed to add type=0 to work on bsc

--- scripts/climb_deploy.py
+++ scripts/climb_deploy.py
@@ -4,14 +4,14 @@ from ape import project, accounts
 def main():
     owner = accounts.load("dev_test")
     print(f"Owner'{owner.address}'")
-    usdt = project.TestToken.deploy("tUSDT", "TUSDT", sender=owner)
-    busd = project.TestToken.deploy("tBUSD", "TBUSD", sender=owner)
+    usdt = project.TestToken.deploy("tUSDT", "TUSDT", sender=owner, type=0)
+    busd = project.TestToken.deploy("tBUSD", "TBUSD", sender=owner, type=0)
 
     climb = project.ClimbTokenV2.deploy(
-        [usdt.address, busd.address], owner.address, sender=owner
+        [usdt.address, busd.address], owner.address, sender=owner, type=0
     )
-    matrix = project.BinanceWealthMatrix.deploy(climb.address, sender=owner)
+    matrix = project.BinanceWealthMatrix.deploy(climb.address, sender=owner, type=0)
     # Initial setup
-    usdt.transfer(climb.address, int(1e18), sender=owner)
-    climb.setMatrixContract(matrix.address, True, sender=owner)
-    matrix.initializeMatrix(sender=owner)
+    usdt.transfer(climb.address, int(1e18), sender=owner, type=0)
+    climb.setMatrixContract(matrix.address, True, sender=owner, type=0)
+    matrix.initializeMatrix(sender=owner, type=0)
