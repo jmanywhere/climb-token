3b54f46c2d50d66f8af6477b12bb0e742e5b5dc6
Semi Invader <=>
2023-06-01 18:32:15 -0600

    stupid wagmi... need to upgrade soon

--- climb-ui/next.config.mjs
+++ climb-ui/next.config.mjs
@@ -7,7 +7,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
